4237c98472b371964902f40575baaf02be4af9e8
samutumba <samutumba@outlook.com>
2022-05-12 20:14:30 -0400

    added checkout page

---
+++ src/pages/checkout.tsx
@@ -0,0 +1,30 @@
+import { NextPage } from "next";
+import { Footer, Meta, NavBar } from "../components";
+import { UseUserValue } from "../hooks/StoreProvider";
+
+const CheckOut: NextPage = () => {
+
+  return (
+    <div className="font-title bg-slate-200 text-md text-white">
+      <Meta
+        title="About: Mumz Care and Kid's Store"
+        keywords={[
+          "About",
+          "MumzCare",
+          "Babies",
+          "Mother's Clothing",
+          "Babies Clothing",
+          "Flavia Katamba",
+        ]}
+        socialTitle="About: Mumz Care and Kid's Store"
+        socialDesc="E-Commerce platform dedicated to providing the Ugandan market with produces for mother's, babies and the odd father."
+        description="E-Commerce platform dedicated to providing the Ugandan market with produces for mother's, babies and the odd father."
+      />
+
+      <h1 className="text-3xl font-bold underline">Check Out !</h1>
+
+    </div>
+  );
+};
+
+export default CheckOut;
